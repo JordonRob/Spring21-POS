54ba6c2304122daa17244cb72b430773420c065c
Jordon R. Roberts <jordonroberts99@yahoo.com>
2021-02-01 19:22:50 -0500

    Javascript
    Added basic javascript functions in preparation for more advanced coding

---
+++ Client/script.js
@@ -0,0 +1,12 @@
+function lockscreen() {
+    alert ("When this button is pressed, User logs out");
+  }
+function management(){
+      alert("When pressed, management-only functions will appear on the right side")
+  }
+function item(){
+    alert("When pressed, item function options will appear on the right side")
+}
+function transaction(){
+    alert("When pressed, transaction functions will appear on the right side")
+}
