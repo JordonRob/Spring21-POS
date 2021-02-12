44aca4f4db5aab1fa7b0d5a381f267991f2ef533
Kyle Robinson <78045799+kyler0b@users.noreply.github.com>
2021-02-12 13:33:38 -0500

    Update db_insert.php

--- backend/db_insert.php
+++ backend/db_insert.php
@@ -2,14 +2,17 @@
 
 include '../backend/db_connection.php';
 
+//Opens connection
 openCon();
-//$first_name = mysqli_real_escape_string($link, $_REQUEST['first_name']);
-//$last_name = mysqli_real_escape_string($link, $_REQUEST['last_name']);
-//$password = mysqli_real_escape_string($link, $_REQUEST['password']);
+
+$first_name = mysqli_real_escape_string($link, $_REQUEST['firstname']);
+$last_name = mysqli_real_escape_string($link, $_REQUEST['lastname']);
+$is_management = mysqli_real_escape_string($link, $_REQUEST['is_management']);
+$password = mysqli_real_escape_string($link, $_REQUEST['password']);
  
 //Attempt insert will add info
-        //$sql = "INSERT INTO USERS(first_name, last_name, password)  
-        //VALUES ('$first_name', '$last_name', '$password')";
+        $sql = "INSERT INTO 'users'(first_name, last_name, password);  
+        VALUES ('$first_name', '$last_name', '$is_management', '$password')";
 
 if(mysqli_query($link, $sql)){
     echo "Records added successfully.";
@@ -17,6 +20,6 @@ if(mysqli_query($link, $sql)){
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 }
  
-// Close connection
-mysqli_close($link);
+//Closes connection
+closeConn($conn);
 ?>
