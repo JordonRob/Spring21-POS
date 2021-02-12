8fcf0c69d73018d2cbeaa5739cade607da84a676
Kyle Robinson <78045799+kyler0b@users.noreply.github.com>
2021-02-12 14:20:34 -0500

    Update logout.php

--- backend/logout.php
+++ backend/logout.php
@@ -1,13 +1,12 @@
 <?php
 
 session_start();
-$_SESSION['user'] = array();
-$id=$_POST['id'];
-$password=$_POST['password'];
-array_push($_SESSION['user'],$id,$password);
-unset($_SESSION['user']);
 
+$_SESSION = array();
+
+session_destroy();
 //redirect to login screen
-header('Location: login.html');
+header('Location: login.php');
+exit();
 
 ?>
