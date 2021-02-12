b7783c1b5251e3c7802d7b3af52e151e66183442
mja00 <teamnirvana2@gmail.com>
2021-02-12 14:36:54 -0500

    Made the index page check for login

--- backend/user_functions.php
+++ backend/user_functions.php
@@ -1,7 +1,8 @@
 <?php
 
 function createUser($conn, $firstname, $lastname, $is_management, $password) {
-    $create_user_query = "INSERT INTO `users`(`firstname`, `lastname`, `is_management`, `password`) VALUES ('{$firstname}','{$lastname}','{$is_management}','{$password}')";
+    $password_hash = password_hash($password, PASSWORD_DEFAULT);
+    $create_user_query = "INSERT INTO `users`(`firstname`, `lastname`, `is_management`, `password`) VALUES ('{$firstname}','{$lastname}','{$is_management}','{$password_hash}')";
     if (!$conn->query($create_user_query) === TRUE) {
         echo "Error: " . $create_user_query . "<br>" . $conn->error;
         if ($conn->error == "Table 'securepos.users' doesn't exist") {
