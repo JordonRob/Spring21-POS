e79988b6097abf74d5a5b634622836380095e18a
Kyle Robinson <78045799+kyler0b@users.noreply.github.com>
2021-02-12 13:38:13 -0500

    Update db_insert.php

--- backend/db_insert.php
+++ backend/db_insert.php
@@ -5,14 +5,14 @@ include '../backend/db_connection.php';
 //Opens connection
 openConn();
 
-$first_name = mysqli_real_escape_string($link, $_REQUEST['firstname']);
-$last_name = mysqli_real_escape_string($link, $_REQUEST['lastname']);
+$firstname = mysqli_real_escape_string($link, $_REQUEST['firstname']);
+$lastname = mysqli_real_escape_string($link, $_REQUEST['lastname']);
 $is_management = mysqli_real_escape_string($link, $_REQUEST['is_management']);
 $password = mysqli_real_escape_string($link, $_REQUEST['password']);
  
 //Attempt insert will add info
         $sql = "INSERT INTO 'users'(first_name, last_name, is_management, password);  
-        VALUES ('$first_name', '$last_name', '$is_management', '$password')";
+        VALUES ('$firstname', '$lastname', '$is_management', '$password')";
 
 if(mysqli_query($link, $sql)){
     echo "Records added successfully.";
