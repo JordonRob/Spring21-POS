02780a49367e33593de4cb13e844fb8a3a64447c
Kyle Robinson <78045799+kyler0b@users.noreply.github.com>
2021-02-11 23:09:30 -0500

    Create session.php

---
+++ backend/session.php
@@ -0,0 +1,9 @@
+<?php
+//start session
+session_start();
+//user logged in check, if yes redirect
+if( isset($_SESSION["id"]) && $_SESSION["id"]== true)
+{
+    //redirect to navigation page goes here
+}
+?>
