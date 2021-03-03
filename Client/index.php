79062b65c62d78bc9f0b5aed463bbc2bc76ac2c6
Jordon R. Roberts <jordonroberts99@yahoo.com>
2021-03-03 00:10:08 -0500

    dbcontroller
    changes
    added db controller
    non functional php for cart

--- Client/index.php
+++ Client/index.php
@@ -33,16 +33,100 @@ require_once "../backend/login_check.php"
 <div class="item-description"><!--This div is for the item description-->
     <form>
         <input type="search" id="pic" name="pic" placeholder="Please enter Product Identification Code 'PIDC'">
+        <button>Add</button>
     </form>
     
     <div class="cartDisplay" id="cartDisplay">
-    <table>
-        
-        
-        
-        
-        </table>
+<?php
+    session_start();
+    require_once "../backend/dbcontroller.php";
+    $db_handle = new DBController();
+    if(!empty($_GET["action"])){
+        switch($_GET["action"]){
+            case "add":
+	   if(!empty($_POST["quantity"])) {
+        $productByCode = $db_handle->runQuery("SELECT * FROM strproducts WHERE PIDC='" . $_GET["PIDC"] . "'");
+           $itemArray = array($productByCode[0]["PIDC"]=>array('name'=>$productByCode[0]["name"], 'PIDC'=>$productByCode[0]["PIDC"], 'price'=>$productByCode[0]["price"]));
+		
+           if(!empty($_SESSION["cart_item"])) {
+               if(in_array($productByCode[0]["PIDC"],array_keys($_SESSION["cart_item"]))) {
+                   foreach($_SESSION["cart_item"] as $k => $v) {
+                       if($productByCode[0]["PIDC"] == $k) {
+                           if(empty($_SESSION["cart_item"][$k]["quantity"])) {
+                               $_SESSION["cart_item"][$k]["quantity"] = 0;
+                           }
+                           $_SESSION["cart_item"][$k]["quantity"] += $_POST["quantity"];
+                       }
+                   }
+               } else {
+                   $_SESSION["cart_item"] = array_merge($_SESSION["cart_item"],$itemArray);
+               }
+           } else {
+               $_SESSION["cart_item"] = $itemArray; 
+           }
+       }
+                break;
+        }
+    }
+?>
+ 
+       <div id="shopping-cart">
+<div class="txt-heading">Shopping Cart</div>
 
+<a id="btnEmpty" href="index.php?action=empty">Empty Cart</a>
+<?php
+if(isset($_SESSION["cart_item"])){
+    $total_quantity = 0;
+    $total_price = 0;
+?>	
+<table class="tbl-cart" cellpadding="10" cellspacing="1">
+<tbody>
+<tr>
+<th style="text-align:left;">Name</th>
+<th style="text-align:left;">Code</th>
+<th style="text-align:right;" width="5%">Quantity</th>
+<th style="text-align:right;" width="10%">Unit Price</th>
+<th style="text-align:right;" width="10%">Price</th>
+<th style="text-align:center;" width="5%">Remove</th>
+</tr>	
+<?php		
+    foreach ($_SESSION["cart_item"] as $item){
+        $item_price = $item["quantity"]*$item["price"];
+		?>
+				<tr>
+				<td><img src="<?php echo $item["image"]; ?>" class="cart-item-image" /><?php echo $item["name"]; ?></td>
+				<td><?php echo $item["code"]; ?></td>
+				<td style="text-align:right;"><?php echo $item["quantity"]; ?></td>
+				<td  style="text-align:right;"><?php echo "$ ".$item["price"]; ?></td>
+				<td  style="text-align:right;"><?php echo "$ ". number_format($item_price,2); ?></td>
+				<td style="text-align:center;"><a href="index.php?action=remove&code=<?php echo $item["code"]; ?>" class="btnRemoveAction"><img src="icon-delete.png" alt="Remove Item" /></a></td>
+				</tr>
+				<?php
+				$total_quantity += $item["quantity"];
+				$total_price += ($item["price"]*$item["quantity"]);
+		}
+		?>
+
+<tr>
+<td colspan="2" align="right">Total:</td>
+<td align="right"><?php echo $total_quantity; ?></td>
+<td align="right" colspan="2"><strong><?php echo "$ ".number_format($total_price, 2); ?></strong></td>
+<td></td>
+</tr>
+</tbody>
+</table>		
+  <?php
+} else {
+?>
+<div class="no-records">Your Cart is Empty</div>
+<?php 
+}
+?>
+</div>
+        
+        
+        
+        
     </div>
     
 
