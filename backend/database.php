8459fdc71868d6018bdd834873f1a60d4540d13f
sanjai00 <78054191+sanjai00@users.noreply.github.com>
2021-03-12 14:19:52 -0500

    Create database.php
    inventoryform

---
+++ backend/database.php
@@ -0,0 +1,12 @@
+
+//used for inventory table
+<?php
+$servername='localhost';
+$username='root';
+$password='';
+$dbname = "securepos";
+$conn=mysqli_connect($servername,$username,$password,"$dbname");
+if(!$conn){
+   die('Could not Connect My Sql:' .mysql_error());
+}
+?>
