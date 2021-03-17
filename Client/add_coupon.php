692897696e9a7f9f7de7f8dffde0e229c6caef2c
Kyle Robinson <78045799+kyler0b@users.noreply.github.com>
2021-03-16 23:06:22 -0400

    Coupon

---
+++ Client/add_coupon.php
@@ -0,0 +1,33 @@
+<?php
+
+
+require_once "../backend/db_connection.php";
+
+//Define empty variables
+$Name = $Code = $Price = "";
+$Name_Error = $Code_Error = $Price_Error = "";
+
+//Process data
+// Check if request is a post
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    //Check if the fields are empty
+    if (empty(trim($_POST["Name"]))) {
+        $Name_Error = "Please enter a product name";
+    } else {
+        $Name = trim($_POST["Name"]);
+    }
+    if (empty(trim($_POST["Code"]))) {
+        $Code_Error = "Please enter a item discount code.";
+    } else {
+        $Code = trim($_POST["Code"]);
+    }
+    if (empty(trim($_POST["Price"]))) {
+        $Price_Error = "Please enter the coupon value.";
+    } else {
+        $Price = trim($_POST["Price"]);
+    }
+}
+
+?>
---
+++ sql/Coupons.sql
@@ -0,0 +1,35 @@
+--
+-- Table structure for table `Coupons`
+--
+CREATE TABLE `Coupons` (
+  `Name` varchar(255) NOT NULL,
+  `Code` varchar(255) NOT NULL,
+  `Price` double(10,2) NOT NULL,
+  `Date Created` DATETIME DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Coupons`
+--
+
+INSERT INTO `Coupons` (`Name`, `Code`, `Price`, `Date Created`) VALUES
+('Apple', '234521', .50, CURRENT_TIMESTAMP);
+
+--
+-- Indexes for table `Coupons`
+--
+ALTER TABLE `Coupons`
+  ADD PRIMARY KEY (`Code`),
+  ADD UNIQUE KEY `Code` (`Code`);
+
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Coupons`
+--
+ALTER TABLE `Coupons`
+  MODIFY `Code` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
