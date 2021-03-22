41154803477dfad1ae0c5bf669caf0a3ed990e73
sanjai00 <78054191+sanjai00@users.noreply.github.com>
2021-03-22 19:31:43 -0400

    Update inventory.php

--- Client/inventory.php
+++ Client/inventory.php
@@ -3,17 +3,17 @@ require_once "../backend/database.php";
 if(isset($_POST['save'])){  //checks to see if the form was submitted
 //variables for each section of form
     $name = $_POST['name'];
-    $PIDC = $_POST['PIDC'];
+    $Code = $_POST['Code'];
     $price = $_POST['price'];
     $quantity = $_POST['quantity'];
 
   
 //insertion into database
    
-       $query = "REPLACE  INTO products(name,PIDC,price,quantity) VALUES('$name', '$PIDC', '$price', '$quantity')";
+       $query = "INSERT INTO products(name,Code,price,quantity) VALUES('$name', '$Code', '$price', '$quantity') ON DUPLICATE KEY UPDATE name='$name', Code='$Code', price='$price', quantity='$quantity'";
        
   //checks to see if
-    if ((mysqli_query($conn, $query) && ("products(name,PIDC)== VALUES('$name', '$PIDC)"))) {
+    if ((mysqli_query($conn, $query))){ //&& (products(name,Code)== VALUES('$name', '$Code')))) {
             echo "New record created successfully !";
          } else {
             echo "Error: " . $query . "
@@ -21,5 +21,20 @@ if(isset($_POST['save'])){  //checks to see if the form was submitted
          }
          mysqli_close($conn);
     }
-       
+if(isset($_POST['remove']))
+    $name = $_POST['name'];
+    $Code = $_POST['Code'];
+    $price = $_POST['price'];
+    $quantity = $_POST['quantity'];
+
+  $sql = "DELETE FROM products WHERE Code=$Code";
+//checks to see if
+if(mysqli_query($conn,$sql)) {
+         echo "New record created successfully !";
+      } else {
+         echo "Error: " . $query . "
+ " . mysqli_error($conn);
+      }
+      mysqli_close($conn);
+ 
     ?>
