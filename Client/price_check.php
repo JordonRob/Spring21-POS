8a03338ddfcc2f43712beb26818028b6a66e9349
jennar305 <71038940+jennar305@users.noreply.github.com>
2021-03-24 02:28:22 -0400

    update price check

--- Client/price_check.php
+++ Client/price_check.php
@@ -1,31 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">   
-
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">  
-<title> Price Check Form</title>
-<link rel="stylesheet" href="price_check.css">
-</head>
-
-<body>
-    <div class="price-check-container">
-
-        <div class = "header">
-                Price Check
-        </div>
-        <div>
-           <form action = " " method = "POST" >
-                <input type = "text" name = "PIDC" class = "btn" placeholder = "ENTER PIDC">
-                <input type="submit" name = "search" class = "btn" value = "Find Price"> 
-            </form> 
-
-        <?php
+<?php
         $connection = mysqli_connect("localhost", "root", "");
         $db = mysqli_select_db($connection, 'securepos');
 
-        if(isset($_POST['search']))
+        if(isset($_GET['PIDC']))
         {
-            $PIDC = $_POST['PIDC'];
+            $PIDC = $_GET['PIDC'];
 
             $query = "SELECT price FROM 'strproducts' where PIDC = '$PIDC'";
             $query_run = mysqli_query($connection, $query);
@@ -34,20 +13,9 @@
             {
                 ?>
 
-                <h1> <?php echo $row['price'] ?> </h1>
+                <h1> <?php echo "Price: " . $row['price'] ?> </h1>
 
                 <?php
             }
         }
-        ?>
-        </div>
-
-
-    <form method = "POST" action = "index.php">
-            <div class="PIDCbox">
-                <input type = "submit" class = "button" id="returnbtn" value = "Return Home">
-            </div>
-    </form>
-
-</body>
-</html>
+?>
