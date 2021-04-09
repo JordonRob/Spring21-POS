301dcae17070506cc921c87cf437167865a93293
Jordon R. Roberts <jordonroberts99@yahoo.com>
2021-04-08 22:14:10 -0400

    Create products_new.sql
    create products_new

---
+++ sql/products_new.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 09, 2021 at 03:55 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `securepos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products_new`
+--
+
+CREATE TABLE `products_new` (
+  `PID` int(6) NOT NULL,
+  `name` text NOT NULL,
+  `upc` int(12) NOT NULL,
+  `VID` int(11) NOT NULL,
+  `quantity` int(3) NOT NULL,
+  `ws_cost` double NOT NULL,
+  `rt_cost` double NOT NULL,
+  `is_taxable` tinyint(1) NOT NULL,
+  `is_perishable` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `products_new`
+--
+ALTER TABLE `products_new`
+  ADD PRIMARY KEY (`PID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `products_new`
+--
+ALTER TABLE `products_new`
+  MODIFY `PID` int(6) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
