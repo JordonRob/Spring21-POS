64f0e788c31a328195cc59de293188f9d9c05fb2
sanjai00 <78054191+sanjai00@users.noreply.github.com>
2021-05-04 14:34:35 -0400

    Update inventory.php

--- Client/inventory.php
+++ Client/inventory.php
@@ -1,15 +1,26 @@
 <?php 
 require_once "../backend/database.php";
+    
 if(isset($_POST['save'])){  //checks to see if the form was submitted
 //variables for each section of form
-    $name = $_POST['name'];
-    $price = $_POST['price'];
+    $name = $_POST['pname'];
+    $sku = $_POST['sku'];
+    $VID = $_POST['VID'];
     $quantity = $_POST['quantity'];
+    $wholesale_price = $_POST['ws_cost'];
+    $retail_price = $_POST['rt_cost'];
+    $taxable = $_POST['is_taxable'];
+    $perishable = $_POST['is_perishable'];
+    
+
+
+
+    
 
   
 //insertion into database
    
-       $query = "REPLACE INTO products(name,price,quantity) VALUES('$name','$price', '$quantity')";
+       $query = "REPLACE INTO products_new(pname,sku,VID,quantity,ws_cost,rt_cost,is_taxable,is_perishable) VALUES('$name','$sku','$VID','$quantity','$wholesale_price','$retail_price','$taxable','$perishable')";
    
        
   //checks to see if any errors
@@ -25,11 +36,11 @@ if(isset($_POST['save'])){  //checks to see if the form was submitted
     
    // REMOVE Function
 if(isset($_POST['remove'])){
-    $name = $_POST['name'];
-    $price = $_POST['price'];
-    $quantity = $_POST['quantity'];
+    $name = $_POST['pname'];
+    //$price = $_POST['price'];
+    //$quantity = $_POST['quantity'];
 
-  $sql = "DELETE FROM products WHERE name='" . $_POST["name"] . "'";
+  $sql = "DELETE FROM products_new WHERE pname='" . $_POST["pname"] . "'";
 //checks to see if any errors
 if(mysqli_query($conn,$sql)) {
          echo "New record created successfully !";
